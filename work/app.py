4f9d269563d106a8e562bb31864c9dbb115325eb
iymarch <61033909+iymarch@users.noreply.github.com>
2021-03-26 17:16:41 +0300

    fixed variable names

--- work/app.py
+++ work/app.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
