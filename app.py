f6c07fea08b98d47c52b8a1c6249bd0cb6ffdc9b
Gromonik <9kloaka@gmail.com>
2021-03-29 17:00:15 +0300

    Отображение данных из бд (мистика цифры 500)

--- app.py
+++ app.py
@@ -27,8 +27,14 @@ def index():
 
 @app.route('/posts')
 def posts():
-    articles = Article.query.order_by(Article.date).all()
+    articles = Article.query.order_by(Article.date.desc()).all()
     return render_template("posts.html", articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def posts_detail(id):
+    article = Article.query.get(id)
+    return render_template("post_detail.html", article=article)
 
 
 @app.route('/about')
@@ -48,7 +54,7 @@ def create_article():
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect('/')
+            return redirect('/posts')
         except:
             return "При добавлении статьи произошла ошибка"
     else:
